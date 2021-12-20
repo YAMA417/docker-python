3700388f8f954f6564b3e8627087c8958491d5b8
YAMA417 <y.m.n.k.t30113@gmail.com>
2021-12-20 22:49:31 +0900

    python演習

---
+++ lesson.py
@@ -0,0 +1,14 @@
+# import lesson_package.utils
+# from lesson_package.tools import utils
+# from lesson_package.utils import say_twice
+
+
+# from lesson_package.talk import human
+# from lesson_package.talk import animal
+# from lesson_package.talk import *
+
+# print(animal.sing())
+# print(animal.cry())
+
+from lesson_package.tools import utils
+print(utils.say_twice('word'))
---
+++ lesson_package/talk/animal.py
@@ -0,0 +1,10 @@
+from lesson_package.tools import utils
+# from ..tools import utils
+
+
+def sing():
+  return 'hufahu hahahuhhjahjdhajhj'
+
+
+def cry():
+  return utils.say_twice('dhauhudhaudhuahduha')
