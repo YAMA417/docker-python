a28825cfc30d3b8312da3bb1f23b2ad24a3384be
YAMA417 <y.m.n.k.t30113@gmail.com>
2021-12-22 19:32:14 +0900

    ファイルの操作

--- lesson.py
+++ lesson.py
@@ -1,14 +1,3 @@
-# import lesson_package.utils
-# from lesson_package.tools import utils
-# from lesson_package.utils import say_twice
-
-
-# from lesson_package.talk import human
-# from lesson_package.talk import animal
-# from lesson_package.talk import *
-
-# print(animal.sing())
-# print(animal.cry())
-
-from lesson_package.tools import utils
-print(utils.say_twice('word'))
+with open('text.txt', 'w')as f:
+  f.write('TrySail\n')
+  print('I am print', file=f)
