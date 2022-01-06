420b817c1d916d7876de95151d7e285090bb8200
YAMA417 <y.m.n.k.t30113@gmail.com>
2022-01-06 21:28:35 +0900

    python基礎

--- lesson.py
+++ lesson.py
@@ -1,3 +1,17 @@
-with open('text.txt', 'w')as f:
-  f.write('TrySail\n')
-  print('I am print', file=f)
+import string
+s = """\
+Hi $name.
+
+$contents
+
+Hava a good day
+"""
+
+t = string.Template(s)
+contents = t.substitute(name='URI', contents='Tyr Wish Us!')
+print(contents)
+# with open('test.txt', 'r+') as f:
+#   # f.write(s)
+#   # 書き込みをしたら初めに戻るようにしないと読み込みが書き込んだ後になる
+#   f.seek(0)
+#   print(f.read())
